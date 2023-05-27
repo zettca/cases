529313503ba70401d66166a9a3467ca64045807d
Bruno <zettca@users.noreply.github.com>
2023-05-27 03:08:11 +0100

    feat: add more cases & styles 💅

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { Component, For, createEffect, createSignal } from "solid-js";
+import { Component, For, createSignal } from "solid-js";
 import {
   camelCase,
-  pascalCase,
+  constantCase,
   capitalCase,
   dotCase,
   headerCase,
+  paramCase,
+  pascalCase,
   pathCase,
+  sentenceCase,
   snakeCase,
 } from "change-case";
 
 import styles from "./App.module.css";
 
 const Case = (props: { label: string; value: string }) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(props.value);
+    console.log("copied", props.value);
+  };
+
   return (
-    <div>
+    <div class={styles.case}>
       <h3>{props.label}</h3>
-      <code>{props.value}</code>
+      <button
+        onClick={handleCopy}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") handleCopy();
+        }}
+      >
+        <code>{props.value}</code>
+      </button>
     </div>
   );
 };
 
 const CASES = {
-  pascalCase,
   camelCase,
+  pascalCase,
+  snakeCase,
+  kebabCase: paramCase,
   capitalCase,
+  constantCase,
   dotCase,
   headerCase,
   pathCase,
-  snakeCase,
+  sentenceCase,
 };
 
 const defaultValue = "case me this";
@@ -35,15 +53,10 @@ const defaultValue = "case me this";
 const App: Component = () => {
   const [value, setValue] = createSignal(defaultValue);
 
-  createEffect(() => {
-    console.log(value());
-  });
-
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <input
-          autofocus
           type="text"
           class={styles.input}
           value={defaultValue}
@@ -52,7 +65,7 @@ const App: Component = () => {
       </header>
       <main class={styles.main}>
         <For each={Object.entries(CASES)}>
-          {([label, fn]) => <Case label={label} value={fn(value())} />}
+          {([label, fn]) => <Case label={fn(label)} value={fn(value())} />}
         </For>
       </main>
     </div>
