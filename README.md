9218b6054500091f97841583f3567d2d291cf93e
Bruno <zettca@users.noreply.github.com>
2023-05-27 01:13:36 +0100

    feat: add initial cases

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Casing
+# Cases
 
-Want to convert to a different case? Forgot what `snake_case` or `kebab-case` is? You've come to the right place. https://zettca.github.io/casing
+Want to convert to a different case? Forgot what `snake_case` or `kebab-case` is? You've come to the right place. https://zettca.github.io/cases
 
 Written in [Solid](https://www.solidjs.com/) because I wanted to try it out. Bootstrapped with [solidjs/templates](https://github.com/solidjs/templates)
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import type { Component } from "solid-js";
+import { Component, For, createEffect, createSignal } from "solid-js";
+import {
+  camelCase,
+  pascalCase,
+  capitalCase,
+  dotCase,
+  headerCase,
+  pathCase,
+  snakeCase,
+} from "change-case";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 
+const Case = (props: { label: string; value: string }) => {
+  return (
+    <div>
+      <h3>{props.label}</h3>
+      <code>{props.value}</code>
+    </div>
+  );
+};
+
+const CASES = {
+  pascalCase,
+  camelCase,
+  capitalCase,
+  dotCase,
+  headerCase,
+  pathCase,
+  snakeCase,
+};
+
+const defaultValue = "case me this";
+
 const App: Component = () => {
+  const [value, setValue] = createSignal(defaultValue);
+
+  createEffect(() => {
+    console.log(value());
+  });
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <input
+          type="text"
+          value={defaultValue}
+          onInput={(event) => setValue(event.currentTarget.value)}
+        />
       </header>
+      <main class={styles.main}>
+        <For each={Object.entries(CASES)}>
+          {([label, fn]) => <Case label={label} value={fn(value())} />}
+        </For>
+      </main>
     </div>
   );
 };
